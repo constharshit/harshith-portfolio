baafcbf3f6e5554e5cfcdcf54fc8ffc03ab7eda3
Harshit Kumar Jain <harshitkumarjain.lnu@usc.edu>
2024-04-01 11:24:20 -0700

    checking accessibility

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,7 +16,7 @@ import useThemeSwitch from "./hooks/useThemeSwitch";
 const CustomLink = ({ href, title, className = "" }) => {
   const router = useRouter();
   return (
-    <Link href={href} className={`${className} relative group`}>
+    <Link name="link-button" href={href} className={`${className} relative group`}>
       {title}
       <span
         className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
